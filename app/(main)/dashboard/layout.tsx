10a7d36759ba9b68ba79b1e337c61d2878b68e1b
dhakarRaghu <rdhakad2002@gmail.com>
2024-09-05 01:10:32 +0530

    create workspace area for user todo

--- app/(main)/dashboard/layout.tsx
+++ app/(main)/dashboard/layout.tsx
@@ -12,8 +12,8 @@ const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
   //if (error) throw new Error();
   return (
     <main className="flex over-hidden h-screen">
-      {/* <SubscriptionModalProvider products={products}>
         {children}
+      {/* <SubscriptionModalProvider products={products}>
      </SubscriptionModalProvider> */}
     </main>
   );
