0c8fb86341aa8da1378cfe3acfee1b66965a091d
dhakarRaghu <rdhakad2002@gmail.com>
2024-09-08 22:23:24 +0530

    components of banner upload form

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -4,8 +4,8 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import db from '@/lib/supabase/db';
 import { redirect } from 'next/navigation';
- import DashboardSetup from '@/components/dashboard-setup/dashboard-setup';
- import { getUserSubscriptionStatus } from '@/lib/supabase/queries';
+import DashboardSetup from '@/components/dashboard-setup/dashboard-setup';
+import { getUserSubscriptionStatus } from '@/lib/supabase/queries';
 
 const DashboardPage = async () => {
   const supabase = createServerComponentClient({ cookies });
